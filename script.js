0df6359e47b9b5692aae99e8745cafef30a70704
Ngaremaina <ngaremaina4@gmail.com>
2023-03-12 15:58:07 +0300

    Portfolio

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ function myBackground(title) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(title).style.display = "block";
-    
+    document.getElementById(title).style.display = "block";   
   }
+
+  // const parent = document.createElement('div');
+  // parent.setAttribute("class", "parent-container");
+
+  // const container = document.createElement('div');
+  // container.setAttribute("class", "bottom-container");
+
+  // const welcome = document.createElement('h1');
+  // welcome.setAttribute("id", "copyright");
+  // welcome.textContent = "Welcome to my Portfolio";
+
+  // document.body.append(parent);
+  // parent.appendChild(container);
+  // container.appendChild(welcome);
+
 
